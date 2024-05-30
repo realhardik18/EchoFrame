9e28c5b5fd6054daa6664decb489dc4aff39507e
realhardik18 <realhardik18@gmail.com>
2024-05-30 21:09:42 +0530

    changing ui

--- src/Card.jsx
+++ src/Card.jsx
@@ -47,7 +47,7 @@ const Card = ({ cardColor, fontColor, fontFamily, isValidLink, embedLink, topTra
             <div className="max-w-md mx-auto bg-black rounded-lg overflow-hidden shadow-lg mt-4">
               <div className="px-4 py-2">
               <h6 className="text-white text-base font-bold mb-2">Top Artists | {currentMonth}</h6>
-                <p className="text-sm text-green-300 mb-4">Made with https://echo-frame.vercel.app/</p>                
+                <p className="text-xs text-green-300 mb-4">Made with https://echo-frame.vercel.app/</p>                
                 <div className="overflow-x-auto">
                   <table className="table-auto w-full text-white">
                     <tbody>
@@ -58,7 +58,7 @@ const Card = ({ cardColor, fontColor, fontFamily, isValidLink, embedLink, topTra
                             <div className="flex items-center">
                               <div className="w-12 h-12 mr-4 overflow-hidden">
                                 <img className="w-full h-full object-cover rounded" src={artist.images[0].url} alt={artist.name} />
-                              </div>
+                              </div>e 
                               <p className="text-xs font-medium">{artist.name}</p>
                             </div>
                           </td>
